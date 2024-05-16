0a34ebf2858aae2a579a8fdd5d4c7d1ea2e90caa
Dennis <53354166+Bejdenn@users.noreply.github.com>
2024-05-16 11:17:53 +0200

    Wrap lines at whole words instead of single chars

--- init.lua
+++ init.lua
@@ -859,5 +859,9 @@ require('lazy').setup({
   },
 })
 
+vim.opt.breakindent = true
+vim.opt.showbreak = string.rep(' ', 3) -- Make it so that long lines wrap smartly
+vim.opt.linebreak = true
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
