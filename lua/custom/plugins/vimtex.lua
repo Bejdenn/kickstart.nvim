03fd768ef1abc69969d5bd4c163a49ce26859969
Dennis Bejze <53354166+Bejdenn@users.noreply.github.com>
2024-05-21 16:35:22 +0200

    Choose PDF reader for vimtex depending on OS

--- lua/custom/plugins/vimtex.lua
+++ lua/custom/plugins/vimtex.lua
@@ -1,7 +1,12 @@
+local pdf_readers = {
+  ['Darwin'] = 'skim',
+  ['Linux'] = 'zathura',
+}
+
 return {
   'lervag/vimtex',
   lazy = false,
   config = function()
-    vim.g.vimtex_view_method = 'zathura'
+    vim.g.vimtex_view_method = pdf_readers[vim.loop.os_uname().sysname]
   end,
 }
