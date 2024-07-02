745e76308b0f0f7e193c8053dd260b3d09549db3
Dennis <53354166+Bejdenn@users.noreply.github.com>
2024-07-02 14:06:48 +0200

    Enable markdown linting

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,6 +7,7 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         sh = { 'shellcheck' },
+        markdown = { 'markdownlint' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
