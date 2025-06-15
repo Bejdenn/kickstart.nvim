c185917aaa744103e4dceba82f2952e1a4630548
Dennis Bejze <53354166+Bejdenn@users.noreply.github.com>
2025-06-15 21:35:06 +0200

    Add support for SQL

--- init.lua
+++ init.lua
@@ -717,6 +717,7 @@ require('lazy').setup({
         sh = { 'shfmt' },
         css = { 'prettier' },
         jsonc = { 'prettier' },
+        sql = { 'sqruff' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
