d92842cf903eafd5c0c76761e0cd9021cdb9faab
Dennis Bejze <53354166+Bejdenn@users.noreply.github.com>
2025-07-11 17:41:21 +0200

    Use avante.nvim instead of codecompanion.nvim

---
+++ lua/custom/plugins/ai.lua
@@ -0,0 +1,35 @@
+return {
+  'yetone/avante.nvim',
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  -- ⚠️ must add this setting! ! !
+  build = function()
+    -- conditionally use the correct build system for the current OS
+    if vim.fn.has 'win32' == 1 then
+      return 'powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false'
+    else
+      return 'make'
+    end
+  end,
+  event = 'VeryLazy',
+  version = false, -- Never set this value to "*"! Never!
+  ---@module 'avante'
+  ---@type avante.Config
+  opts = {
+    -- add any opts here
+    -- for example
+    provider = 'copilot',
+  },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+    'zbirenbaum/copilot.lua', -- for providers='copilot'
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { 'markdown', 'Avante' },
+      },
+      ft = { 'markdown', 'Avante' },
+    },
+  },
+}
