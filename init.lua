d732d10d70c6017727429bb314eff21431288605
Dennis Bejze <53354166+Bejdenn@users.noreply.github.com>
2025-08-13 00:12:52 +0200

    fix: remove lazy icons fallback

--- init.lua
+++ init.lua
@@ -177,23 +177,6 @@ rtp:prepend(lazypath)
 require('lazy').setup({
   { import = 'custom.plugins' },
 }, {
-  ui = {
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
   performance = {
     rtp = {
       disabled_plugins = {
