61069137fe434bc7f091f9994bf71f5598832392
Dennis Bejze <53354166+Bejdenn@users.noreply.github.com>
2025-08-13 19:10:00 +0200

    chore(todo-comments): remove obsolete color override

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -2,5 +2,5 @@ return {
   'folke/todo-comments.nvim',
   event = 'VimEnter',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = { signs = false, colors = { default = { 'purple', '#7C3AED' } } },
+  opts = { signs = false },
 }
