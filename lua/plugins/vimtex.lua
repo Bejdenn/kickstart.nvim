39dfa61211dad53fa1c4b50c40d76ef1ba4b38f5
Dennis Bejze <53354166+Bejdenn@users.noreply.github.com>
2025-08-13 18:51:00 +0200

    chore: change `vim.loop` to `vim.uv`

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -8,7 +8,7 @@ return {
     'lervag/vimtex',
     lazy = false,
     config = function()
-      vim.g.vimtex_view_method = pdf_readers[vim.loop.os_uname().sysname]
+      vim.g.vimtex_view_method = pdf_readers[vim.uv.os_uname().sysname]
       vim.g.vimtex_complete_enabled = 1
     end,
   },
