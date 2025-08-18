c9b08afe97b47fc446cdfc7315fd9b836b699766
Dennis Bejze <53354166+Bejdenn@users.noreply.github.com>
2025-08-18 12:44:20 +0200

    feat: switch out autopairs for mini.pairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -32,6 +32,7 @@ return { -- Collection of various small independent plugins/modules
     end
 
     require('mini.sessions').setup()
+    require('mini.pairs').setup()
 
     -- ... and there is more!
     --  Check out: https://github.com/echasnovski/mini.nvim
